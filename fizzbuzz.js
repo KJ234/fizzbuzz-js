e089bc14d6c3d574b3b09452ce848742f8e9b8c0
KJ234 <komaljaved109@gmail.com>
2022-09-26 13:57:43 +0100

    multiple of 3 and 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,6 +8,38 @@ function fizzbuzz() {
     // Put your code here...
 
     
+    
+    for(let i=1; i<101;  i++)
+    {
+        if(i % 3 == 0)
+        console.log('fizz') 
+    
+        
+
+        else if(i % 5 == 0)
+        console.log('buzz') 
+
+        
+    
+        
+        else if(i % 5 == 0 & i % 3 == 0)
+        console.log('fizzbuzz') 
+    
+        else {
+            console.log(i)
+        }
+
+        
+    
+    }
+    
+
+    
+
+
+
+
+    
 }
 
 // Now we run the main function...
