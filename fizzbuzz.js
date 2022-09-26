ae65349fec467a71e8473e54e4eb63f0b1be5190
KJ234 <komaljaved109@gmail.com>
2022-09-26 14:31:18 +0100

    divisible by 15

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,23 +8,16 @@ function fizzbuzz() {
     // Put your code here...
 
     
-    
     for(let i=1; i<101;  i++)
     {
-        if(i % 3 == 0)
-        console.log('fizz') 
-    
-        
-
-        else if(i % 5 == 0)
-        console.log('buzz') 
-
-        
-    
-        
-        else if(i % 5 == 0 & i % 3 == 0)
-        console.log('fizzbuzz') 
-    
+        if (i % 15 == 0){
+            console.log('fizzbuzz')
+        }
+        else if (i % 3 == 0) {
+            console.log('fizz') 
+        } else if(i % 5 == 0) {
+            console.log('buzz') 
+        }
         else {
             console.log(i)
         }
